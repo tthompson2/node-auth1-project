81355f05fc8d63b5eae8b718ba4531b801858649
Trevor Thompson <Thompson_Trevor@bah.com>
2020-05-16 19:51:15 -0400

    Refactored code to take our router functions for non autheticated users and put the functions in the autheticated user section

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,3 +2,57 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model');
 
+router.post('/register', (req, res) => {
+    
+    const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
+
+router.post('/login', (req, res) => {
+    let {username, password } = req.body;
+
+    Users.findBy({username}) 
+    .first()
+    .then(user => {
+
+        if(user && bcrypt.compareSync(password, userpassword)) {
+            req.session.user = username;
+            res.status(200).json({message: `Welcome ${user.username}!`});
+        } else {
+            res.status(401).json({mesasge: 'invalid credentials'});
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+router.delete('/logout', (req, res) => {
+    
+    if(req.session) {
+        req.session.destroy((err) => {
+            if(err) {
+                res.status(400).json({message: 'error logging out:', error:err});
+            } else {
+                res.json({message: 'logged out'});
+            }
+    })
+   } else {
+       res.end();
+   }
+})
+
+// wow, now I feel really silly. The module.exports function is used to
+// export all of the modules attached to the function router
+
+module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -14,34 +14,34 @@ router.get("/users", (req, res) => {
 
 });
 
-router.post("/register", (req, res) => {
-    console.log(req.body)
-    const {username, password} =req.body;
-    const hash = bcrypt.hashsync(password, 8);
-    Users.add(req.body)
-    .then(users => {
-        res.json(users);
-    }).catch(error => {
-        res.send(error);
-    })
-})
+// router.post("/register", (req, res) => {
+//     console.log(req.body)
+//     const {username, password} =req.body;
+//     const hash = bcrypt.hashsync(password, 8);
+//     Users.add(req.body)
+//     .then(users => {
+//         res.json(users);
+//     }).catch(error => {
+//         res.send(error);
+//     })
+// })
 
-router.post("/login", (req, res) => {
+// router.post("/login", (req, res) => {
 
-    let = { username, password} = req.body;
+//     let = { username, password} = req.body;
 
-    Users.findBy({ username }) 
-       .first()
-       .then(user => {
-           if(user && bcrypt.compareSync(password, user.password)) {
-               res.status(200).json({ message: `Welcome ${user.username}!`});
-           } else {
-               res.status(401).json({message: "Invalid Credentials"});
-           }
-           })
-        .catch(error => {
-            res.status(500).json(error);
-        })
-})
+//     Users.findBy({ username }) 
+//        .first()
+//        .then(user => {
+//            if(user && bcrypt.compareSync(password, user.password)) {
+//                res.status(200).json({ message: `Welcome ${user.username}!`});
+//            } else {
+//                res.status(401).json({message: "Invalid Credentials"});
+//            }
+//            })
+//         .catch(error => {
+//             res.status(500).json(error);
+//         })
+// })
 
 module.exports = router;
